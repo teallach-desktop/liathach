604278e8079d238b59a063dba1c7e1a8f41b5816
Johan Malm <jgm323@gmail.com>
2024-02-27 21:28:51 +0000

    Add 01-start-qemu.sh

---
+++ 01-start-qemu.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+url=https://uk.alpinelinux.org/alpine/v3.19/releases/x86_64/alpine-standard-3.19.1-x86_64.iso
+filename=alpine.qcow2
+
+if ! [ -f $(basename $url) ]; then
+	curl -O ${url}
+fi
+
+if ! [ -f $filename ]; then
+	qemu-img create -f qcow2 alpine.qcow2 16G
+fi
+
+qemu-system-x86_64 \
+	-enable-kvm \
+	-m 2048 \
+	-nic user,model=virtio \
+	-drive file=$filename,media=disk,if=virtio \
+	-cdrom $(basename $url) \
+	-display gtk \
+	-cpu host \
+	-vga qxl
+
